db28b34cdd4dd79f3452f3dc48d47d0cc2d8f100
2689737232 <2689737232@qq.com>
2020-09-11 08:24:01 +0800

    初始化提交

---
+++ yeye.py
@@ -0,0 +1,19 @@
+import random as randomObj
+import math
+
+print(randomObj.random())
+i = 45
+print(type(i))
+x = '''三引号 '''
+dictionary = {
+    "name": "lala",
+    "age": 21
+}
+print(dictionary.get("name")) 
+lala  = {
+   "name": "lala"
+}
+x = 2
+print(x ** 8)
+print(isinstance(3,int)) 
+print(7 < 8 > 5)
